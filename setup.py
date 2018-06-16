3e3fda115a85f14965635f9d2c47a910d3a9ef13
GitHub <noreply@github.com>
2018-06-16 14:18:11 +0530

    added print statement in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 with open('LICENSE') as f:
     license = f.read()
-print('hi ...')
+
 setup(name='demo',
       version='1.0',
       description='Python Distribution Utilities',
